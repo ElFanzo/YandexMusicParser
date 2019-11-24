cab933eae730cb2e890b7025ef68b2dfeefc9895
Fanzil <fan.hakimov2010@yandex.ru>
2019-11-24 18:37:40 +0500

    Reorder dict items

--- log.py
+++ log.py
@@ -1,26 +1,24 @@
-msg = {}
-msg["ERR_NET"] = (
-    "Internet is not available. Please, check your connection and try again."
-)
-msg["ERR_NET_BUT_CACHE"] = (
-    f"{msg['ERR_NET']} You can still get your data from the local cache."
-)
-msg["ERR_UPD"] = "Update method is not available for this object."
-msg["ERR_USER"] = "The user '{}' does not exist!"
-msg["ERR_ACCESS"] = "The account of the user '{}' is private!"
-
-msg["UPD"] = "Please, wait. Your data is being updated..."
-
-msg["CACHE_LOOK"] = "Looking for the local cache..."
-msg["CACHE_SUCCESS"] = (
-    "Successfully! Your playlist's data has been read from the cache."
-)
-msg["CACHE_FAIL"] = (
-    "The cache wasn't found. Your playlist's data will be retrieved "
-    "from the Internet and cached to the disc."
-)
-
-msg["DONE"] = "Done!"
+msg = {
+    "DB_DOWNLOAD": "Please, wait. Your data is being downloading...",
+    "DB_FAIL": (
+        "The user wasn't found. All the data will be retrieved "
+        "from the Internet and saved to the database."
+    ),
+    "DB_SEARCH": "Looking for the user in the database...",
+    "DONE": "Done!",
+    "ERR_ACCESS": "The account of the user '{}' is private!",
+    "ERR_BUT_DB": "You can still get your data from the database.",
+    "ERR_NET": (
+        "Internet is not available. "
+        "Please, check your connection and try again."
+    ),
+    "ERR_UPD": "Update method is not available for this object.",
+    "ERR_USER": "The user '{}' does not exist!",
+    "UPD": "Please, wait. Your data is being updated...",
+    "USER_SUCCESS": (
+        "Successfully! The user's data is accessed in the 'user' attribute."
+    ),
+}
 
 
 def flash(**kwargs):
